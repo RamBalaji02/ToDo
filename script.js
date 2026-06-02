f88ac783368d034595f0a3949a1bead06e15a1d6
RamBalaji02 <rbalajis222@gmail.com>
2026-06-02 18:59:22 +0530

    Initial commit - Smart ToDo App

---
+++ script.js
@@ -0,0 +1,124 @@
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+const taskCount = document.getElementById("taskCount");
+const themeBtn = document.getElementById("themeBtn");
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+let currentFilter = "all";
+
+/* SAVE */
+function saveTasks(){
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+/* COUNT */
+function updateCount(){
+    const active = tasks.filter(t => !t.completed).length;
+    taskCount.textContent = active + " Tasks Left";
+}
+
+/* RENDER */
+function renderTasks(){
+
+    taskList.innerHTML = "";
+
+    let filtered = tasks;
+
+    if(currentFilter === "active"){
+        filtered = tasks.filter(t => !t.completed);
+    }
+
+    if(currentFilter === "completed"){
+        filtered = tasks.filter(t => t.completed);
+    }
+
+    filtered.forEach(task => {
+
+        const li = document.createElement("li");
+
+        li.innerHTML = `
+            <div class="task">
+                <span class="${task.completed ? "completed" : ""}">
+                    ${task.text}
+                </span>
+            </div>
+
+            <div class="actions">
+                <button class="complete-btn"><i class="fa-solid fa-check"></i></button>
+                <button class="delete-btn"><i class="fa-solid fa-trash"></i></button>
+            </div>
+        `;
+
+        li.querySelector(".complete-btn").onclick = () => {
+            task.completed = !task.completed;
+            saveTasks();
+            renderTasks();
+        };
+
+        li.querySelector(".delete-btn").onclick = () => {
+            tasks = tasks.filter(t => t.id !== task.id);
+            saveTasks();
+            renderTasks();
+        };
+
+        taskList.appendChild(li);
+    });
+
+    updateCount();
+}
+
+/* ADD TASK */
+function addTask(){
+
+    const text = taskInput.value.trim();
+
+    if(!text){
+        alert("Please enter a task");
+        return;
+    }
+
+    tasks.push({
+        id: Date.now(),
+        text,
+        completed: false
+    });
+
+    taskInput.value = "";
+    saveTasks();
+    renderTasks();
+}
+
+/* ENTER KEY */
+taskInput.addEventListener("keypress", (e) => {
+    if(e.key === "Enter"){
+        addTask();
+    }
+});
+
+/* CLEAR COMPLETED */
+function clearCompleted(){
+    tasks = tasks.filter(t => !t.completed);
+    saveTasks();
+    renderTasks();
+}
+
+/* FILTER */
+function filterTasks(type, btn){
+
+    currentFilter = type;
+
+    document.querySelectorAll(".filters button")
+        .forEach(b => b.classList.remove("active"));
+
+    btn.classList.add("active");
+
+    renderTasks();
+}
+
+/* DARK MODE */
+themeBtn.onclick = () => {
+    document.body.classList.toggle("dark");
+};
+
+/* INIT */
+renderTasks();
